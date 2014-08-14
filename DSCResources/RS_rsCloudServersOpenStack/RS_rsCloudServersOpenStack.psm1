7a0b04399ee20a3e4c8cabdfe53230975c82ed9d
LCZ2RCS1 <mark.key@rackspace.com>
2014-08-14 08:53:30 -0500

    added -join '\' to environmentname path

--- DSCResources/RS_rsCloudServersOpenStack/RS_rsCloudServersOpenStack.psm1
+++ DSCResources/RS_rsCloudServersOpenStack/RS_rsCloudServersOpenStack.psm1
@@ -160,7 +160,7 @@ Function Create-MonitoringEntity {
             if(Test-Path -Path ("C:\Program Files\WindowsPowerShell\DscService\Configuration\" + $server.id + ".mof")) {
                Remove-Item ("C:\Program Files\WindowsPowerShell\DscService\Configuration\" + $server.id + "*") -Force
             }
-            & $($d.wD, $d.mR, $($EnvironmentName + ".ps1")) -Node $server.name -ObjectGuid $server.id -MonitoringID $server.id -MonitoringToken $agentToken
+            & $($d.wD, $d.mR, $($EnvironmentName + ".ps1" -join '\')) -Node $server.name -ObjectGuid $server.id -MonitoringID $server.id -MonitoringToken $agentToken
             Write-EventLog -LogName DevOps -Source RS_rsCloudServersOpenStack -EntryType Information -EventId 1000 -Message "Hash Mismatch: Creating MOF file for server $($server.name) $($server.id)"
          }
          catch {
@@ -241,7 +241,7 @@ Function Create-Mofs {
          Write-EventLog -LogName DevOps -Source RS_rsCloudServersOpenStack -EntryType Information -EventId 1000 -Message "Creating MOF file for server $($server.serverName) `n  $agentToken `n $currentMofs `n $($server.id)"
          Remove-Item ("C:\Program Files\WindowsPowerShell\DscService\Configuration\" + $server.guid + "*") -Force
          ## remove scriptdata and add dynamic clientdsc
-         & $($d.wD, $d.mR, $($EnvironmentName + ".ps1")) -Node $server.serverName -ObjectGuid $server.guid -MonitoringID $server.guid -MonitoringToken $agentToken
+         & $($d.wD, $d.mR, $($EnvironmentName + ".ps1" -join '\')) -Node $server.serverName -ObjectGuid $server.guid -MonitoringID $server.guid -MonitoringToken $agentToken
          #& $(Join-Path $scriptData.Directory.scriptsRoot -ChildPath ClientDSC.ps1) -Node $server.name -ObjectGuid $server.id
       }
       catch {
@@ -384,6 +384,7 @@ Function Get-TargetResource
         decryptionKey = $decryptionKey
         Ensure = $Ensure
         BuildTimeOut = $BuildTimeOut
+        EnvironmentName = $EnvironmentName
 
     }
    
@@ -525,7 +526,7 @@ Function Set-TargetResource
                if(Test-Path -Path ("C:\Program Files\WindowsPowerShell\DscService\Configuration\" + $createServer.server.id + ".mof")) {
                   Remove-Item ("C:\Program Files\WindowsPowerShell\DscService\Configuration\" + $createServer.server.id + "*") -Force
                }
-               powershell.exe $($d.wD, $d.mR, $($EnvironmentName + ".ps1")) -Node $missingServer, -ObjectGuid $createServer.server.id
+               powershell.exe $($d.wD, $d.mR, $($EnvironmentName + ".ps1" -join '\')) -Node $missingServer, -ObjectGuid $createServer.server.id
                #& $(Join-Path $scriptData.Directory.scriptsRoot -ChildPath ClientDSC.ps1) -Node $server.name -ObjectGuid $missingServer.id -MonitoringID $missingServer.id -MonitoringToken $agentToken
                
                Write-EventLog -LogName DevOps -Source RS_rsCloudServersOpenStack -EntryType Information -EventId 1000 -Message "Creating MOF file for server $missingServer"
